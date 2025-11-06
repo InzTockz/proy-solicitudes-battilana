3c170d1fa542ade4e8c9b4d9fa96027ed9fdd340
Fredy <inztockz@gmail.com>
2025-11-06 17:29:03 -0500

    Actualizando el DraftDocumentLine

--- src/main/java/com/battilana/solicitud/pedidos/dtos/DraftDocumentLineRequest.java
+++ src/main/java/com/battilana/solicitud/pedidos/dtos/DraftDocumentLineRequest.java
@@ -3,7 +3,6 @@ package com.battilana.solicitud.pedidos.dtos;
 public record DraftDocumentLineRequest (
         String ItemCode,
         String Quantity,
-        String TaxCode,
-        String UnitPrice
+        String TaxCode
 ){
 }
