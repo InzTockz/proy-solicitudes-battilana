ddeca320a0413ea3a631a2762bdaf6631526c446
Fredy <inztockz@gmail.com>
2026-02-09 12:18:47 -0500

    prod

---
+++ target/generated-sources/annotations/com/battilana/solicitud/pedidos/mapper/AlmacenesMapperImpl.java
@@ -0,0 +1,50 @@
+package com.battilana.solicitud.pedidos.mapper;
+
+import com.battilana.solicitud.pedidos.dtos.AlmacenResponse;
+import com.battilana.solicitud.pedidos.entities.AlmacenesEntity;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2026-02-09T12:17:50-0500",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.5 (Eclipse Adoptium)"
+)
+@Component
+public class AlmacenesMapperImpl implements AlmacenesMapper {
+
+    @Override
+    public List<AlmacenResponse> toAlmacenResponseList(List<AlmacenesEntity> almacenesEntities) {
+        if ( almacenesEntities == null ) {
+            return null;
+        }
+
+        List<AlmacenResponse> list = new ArrayList<AlmacenResponse>( almacenesEntities.size() );
+        for ( AlmacenesEntity almacenesEntity : almacenesEntities ) {
+            list.add( toAlmacenResponse( almacenesEntity ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public AlmacenResponse toAlmacenResponse(AlmacenesEntity almacenesEntity) {
+        if ( almacenesEntity == null ) {
+            return null;
+        }
+
+        Integer idAlmacen = null;
+        String codigo = null;
+        String descripcion = null;
+
+        idAlmacen = almacenesEntity.getIdAlmacen();
+        codigo = almacenesEntity.getCodigo();
+        descripcion = almacenesEntity.getDescripcion();
+
+        AlmacenResponse almacenResponse = new AlmacenResponse( idAlmacen, codigo, descripcion );
+
+        return almacenResponse;
+    }
+}
